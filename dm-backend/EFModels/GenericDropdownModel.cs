e6b16068714399e243e568d987690c952cbbe1e8
namit79 <60868884+namit79@users.noreply.github.com>
2020-05-27 19:12:43 +0530

    changes in dropdown model

--- dm-backend/EFModels/GenericDropdownModel.cs
+++ dm-backend/EFModels/GenericDropdownModel.cs
@@ -6,15 +6,10 @@ namespace dm_backend.EFModels
     public class GenericDropdownModel
     {
         public int Id { get; set; }
-        public string Value { get; set; }
-        internal AppDb Db { get; set; }
-        public GenericDropdownModel()
+        public string Name { get; set; }
+              public GenericDropdownModel()
         {
             
-        }
-         internal GenericDropdownModel(AppDb db)
-        {
-            Db = db;
         }
 
     }
