336979dbc6fbc53378802dea0d09b155b5cc4e50
kaustubh-ex2 <kdhasmana@ex2india.com>
2020-06-01 00:32:46 +0530

    Clean up notification controller

--- dm-backend/Controllers/NotificationController.cs
+++ dm-backend/Controllers/NotificationController.cs
@@ -7,11 +7,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using dm_backend.Logics;
-using dm_backend.Models;
 using Microsoft.AspNetCore.Authorization;
-using MySql.Data.MySqlClient;
 using dm_backend.Data;
+using dm_backend.EFModels;
 using Newtonsoft.Json;
 
 namespace dm_backend.Controllers
@@ -21,58 +19,30 @@ namespace dm_backend.Controllers
     [Route("api/[controller]")]
     public class NotificationController : ControllerBase
     {
-        public AppDb Db { get; }
         public INotificationRepository _repo;
-        public ISendMail _send;
-        public NotificationController( INotificationRepository repo,ISendMail mail)
+        public NotificationController(INotificationRepository repo)
         {
             _repo = repo;
-            _send = mail;
         }
-        // private readonly EFRepository _context;
-
-        // public NotificationController(AppDb db,EFDbContext context)
-        // {
-        //     Db = db;
-        //     _context=context;
-        // }
-        
         [HttpPost]
-        public IActionResult PostMultipleNotifications([FromBody]MultipleNotifications item)
+        public IActionResult PostMultipleNotifications([FromBody]List<Notification> item)
         {
             try
             {
-                Task.WhenAll(_repo.sendMultipleMail(item));
+                _repo.AddMultipleNotifications(item);
+                return Ok();
             }
             catch
             {
                 return BadRequest();
             }
-            var result = _repo.AddMultipleNotifications(item);
-           
-            return new OkObjectResult(item);
         }
 
         
         [HttpGet]
-        public IActionResult GetNotification()
+        public IActionResult GetNotification([FromQuery]BaseQueryParams Qparams)
         {
-             int userId = -1;
-            string searchField = (string)HttpContext.Request.Query["search"] ?? "";
-             string sortField = (string)HttpContext.Request.Query["sort"] ?? "notification_date";
-             string sortDirection = (string)HttpContext.Request.Query["direction"] ?? "asc";
-            if (!string.IsNullOrEmpty(HttpContext.Request.Query["id"]))
-                userId = Convert.ToInt32((string)HttpContext.Request.Query["id"]);
-            int pageNumber = Convert.ToInt32((string)HttpContext.Request.Query["page"]);
-            int pageSize = Convert.ToInt32((string)HttpContext.Request.Query["page-size"]);
-            sortDirection = (sortDirection.ToLower()) == "asc" ? "ASC" : "DESC";
-            
-            // Db.Connection.Open();
-            // var NotificationObject = new NotificationModel(Db);
-            // var pager = PagedList<NotificationModel>.ToPagedList(NotificationObject.GetNotifications(userId, sortField, sortDirection, searchField), pageNumber, pageSize);
-            // Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(pager.getMetaData()));
-            // Db.Connection.Close();
-            var result = _repo.GetAllNotifications(userId, sortField, sortDirection, searchField);
+            var result = _repo.GetAllNotifications(Qparams);
             return Ok(result);
         }
 
@@ -90,7 +60,7 @@ namespace dm_backend.Controllers
         [Route("Count/{id}")]
         public IActionResult GetCount(int id)
          {
-            var result = _repo.GetNotification(id);
+            var result = _repo.GetNotificationCount(id);
             return Ok(result);
         }
 
